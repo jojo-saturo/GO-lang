1b0b4b104af9511bb2a2adfa280c12536549f37d
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-06-05 16:04:27 +0000

    GO variblles

--- main.go
+++ main.go
@@ -1,20 +1,65 @@
+/*
+// Declearing varibles
+package main
+import ("fmt")
+var a int
+var b int = 2
+var c int = 3
+func main () {
+	a = 1
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
+
+// Declearing multiple variables
 package main
 
-import "fmt"
-
-var studentName string
-var studentNum int
-var studentGrade float32
-var student bool
+import (
+	"fmt"
+)
 
 func main() {
-	studentName = "Jojo Saturo"
-	studentNum = 500
-	studentGrade = 4.5
-	student = true
+	var a, b, c, d int = 2, 0, 0, 3
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
 
-	fmt.Println(studentName)
-	fmt.Println(studentNum)
-	fmt.Print(studentGrade)
-	fmt.Println(student)
+// Declearing multiple with int and stirng
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b = 2, "Jojo"
+	c, d := 8, "Saturo"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+} */
+
+// Declearation in Block
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a int
+		b string  = "Jojo saturo BD"
+		c bool    = true
+		d float32 = 28.06
+	)
+	a = 2003
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
 }
