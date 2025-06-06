5d2c86bcb1e9f71562721b548b23aeb8ce9e9456
jojo-saturo <niyitripplejoejoejoe@gmail.com>
2025-06-06 09:39:23 +0100

    learning Print()

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
-// GO Constants
 package main
 
 import (
 	"fmt"
 )
 
-const FNAME, LNAME = "Josiah", "Olaniyi"
-const AGE int = 21
-const GENDER string = "Male"
-const COMP string = "Dark"
+const FNAME = "Josiah"
+const LNAME = "Olaniyi"
+const AGE = 21
+const ABT = "A young dev developing his skills with GO"
 
 func main() {
-	fmt.Println(FNAME, "", LNAME) // making the name on a single line
-	fmt.Println(AGE)
-	fmt.Println(GENDER)
-	fmt.Println(COMP)
-} 
-/*
-// Making a new line with const
-package main
-import ("fmt")
-const FNAME, LNAME = "Josiah", "Olaniyi" 
-const AGE =  21
-const mSTATUS = "Single"
-func main () {
-	fmt.Println(FNAME, "\n") // Adding a space vertically
-	fmt.Println(LNAME)
-	fmt.Println(AGE)
-	fmt.Println(mSTATUS)
-} 
-
-
-// Using var
-package main
-import ("fmt")
-var j, o = "Josiah", "Olaniyi"
-func main() {
-	fmt.Println(j, "", o, "\n") //adding extra space ("")
-	fmt.Println(o, "\n") // adding new line
-	fmt.Println(j, "\n") // same here
-	fmt.Println(o,j) // displayed inversely
-} */
+	fmt.Print(FNAME, " ", LNAME,"\n")
+	fmt.Print(AGE, "\n")
+	fmt.Print(ABT)
+}
+/* Using of Print() doesn't add space and new line automatically, you need add a string for space(" ")
+and also you need "\n" for new line */
